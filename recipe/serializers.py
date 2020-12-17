31a3ebeabbb7e5b15e5a58957f74990136fa17bf
Md. Mehedi Hasan <71551839+MehediKhokon@users.noreply.github.com>
2020-12-17 22:00:24 +0600

    Update serializers.py

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -5,4 +5,4 @@ from .models import Recipe
 class RecipeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Recipe
-		fields = ['title', 'description', 'created']
+		fields = ['id', 'title', 'description', 'created']
